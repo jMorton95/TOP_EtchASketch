9e541a326135f5e9b3b86f688cc35d40ad07e555
unknown <mortson@hotmail.co.uk>
2022-03-16 01:26:39 +0000

    Found a solution to display the grid as expected, began writing a function to find elements that are clicked on

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const gridContainer = document.querySelector('#gridContainer');
 
 let gridItem;
-let gridSideSize = 5;
+let gridSideSize = 16;
 
-for (i = (gridSideSize * gridSideSize); i > 0; i--){
+for (i = (gridSideSize ** 2); i > 0; i--){
     gridItem = document.createElement('div');
     gridItem.classList.add('grid-item');
-    gridItem.textContent = (gridSideSize * gridSideSize) - (i - 1);
+    //gridItem.textContent = "I"
     gridContainer.appendChild(gridItem);
 
 }
 
+// MAKE FUNCTION THAT FINDS ID OF THE DIV CLICKED INSIDE THE GRID, AND SET ITS BACKGROUND COLOR TO BLACK.
 
+/*const gridClick = document.querySelectorAll('.grid-item');
+gridClick.foreach ((gridClick) => {
+    
+
+}
+*/
